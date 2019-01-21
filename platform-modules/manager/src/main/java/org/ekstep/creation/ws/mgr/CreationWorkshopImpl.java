d1c9592fc9acbe82e6b2e963906afe03cdd46c48
Kumar Gauraw <gaurawk@ilimi.in>
2019-01-21 21:16:11 +0530

    Issue #SC-0000 feat: Add Controller for dev con

--- platform-modules/manager/src/main/java/org/ekstep/creation/ws/mgr/CreationWorkshopImpl.java
+++ platform-modules/manager/src/main/java/org/ekstep/creation/ws/mgr/CreationWorkshopImpl.java
@@ -2,12 +2,16 @@ package org.ekstep.creation.ws.mgr;
 
 import org.ekstep.common.dto.Response;
 import org.ekstep.common.mgr.BaseManager;
+import org.ekstep.common.util.HttpRestUtil;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class CreationWorkshopImpl extends BaseManager {
+
+    private static String REGISTRY_URL = "http://localhost:8080/read-dev"
 
     public Response login(String id) throws Exception{
         Response resp = null;
@@ -15,7 +19,18 @@ public class CreationWorkshopImpl extends BaseManager {
     }
 
     public Response getProfile(String id, String type){
+
         Response resp = null;
+        Map<String,Object> map = new HashMap<String,Object>();
+        Map<String,Object> data = new HashMap<String,Object>();
+        Map<String,String> headerParam = new HashMap<String,String>();
+        data.put("code",id);
+        map.put("request",map);
+        try{
+            resp = HttpRestUtil.makePostRequest(REGISTRY_URL, map, headerParam);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return resp;
     }
 
