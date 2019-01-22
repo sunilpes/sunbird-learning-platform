c6e4a90ea697d3daa856f0a6008dd03f09ee7dc0
Kumar Gauraw <gaurawk@ilimi.in>
2019-01-22 17:45:21 +0530

    Issue #SC-0000 feat: Updated Code

--- platform-modules/manager/src/main/java/org/ekstep/creation/ws/mgr/CreationWorkshopImpl.java
+++ platform-modules/manager/src/main/java/org/ekstep/creation/ws/mgr/CreationWorkshopImpl.java
@@ -1,11 +1,15 @@
 package org.ekstep.creation.ws.mgr;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ekstep.common.dto.Response;
 import org.ekstep.common.mgr.BaseManager;
 import org.ekstep.common.util.HttpRestUtil;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -13,57 +17,146 @@ public class CreationWorkshopImpl extends BaseManager {
 
     private static String REGISTRY_URL = "http://localhost:8080/read-dev";
     private static String ML_URL = "http://localhost:8080/ml";
+    private static String LP_SEARCH = "https://dev.ekstep.in/api/search/v3/search";
 
-    public Response login(Map<String, Object> reqMap) throws Exception{
+    private static ObjectMapper mapper = new ObjectMapper();
+
+
+    public Response login(Map<String, Object> reqMap) throws Exception {
         Response resp = null;
-        try{
-            resp = HttpRestUtil.makePostRequest(REGISTRY_URL, reqMap, new HashMap<String,String>());
-        }catch(Exception e){
+        try {
+            resp = HttpRestUtil.makePostRequest(REGISTRY_URL, reqMap, new HashMap<String, String>());
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return resp;
     }
 
-    public Response getProfile(String id, String type){
+    public Response getProfile(String id, String type) {
 
         Response resp = null;
-        Map<String,Object> map = new HashMap<String,Object>();
-        Map<String,Object> data = new HashMap<String,Object>();
-        Map<String,String> headerParam = new HashMap<String,String>();
-        data.put("code",id);
-        map.put("request",map);
-        try{
+        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> data = new HashMap<String, Object>();
+        Map<String, String> headerParam = new HashMap<String, String>();
+        data.put("code", id);
+        map.put("request", map);
+        try {
             resp = HttpRestUtil.makePostRequest(REGISTRY_URL, map, headerParam);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return resp;
     }
 
-    public Response createFramework(Map<String, Object> map, String channelId){
+    public Response createFramework(Map<String, Object> map, String channelId) {
         Response resp = null;
         return resp;
     }
 
-    public Response generate(Map<String, Object> map, String type){
+    public Response generate(Map<String, Object> map, String type) {
         Response resp = null;
         return resp;
     }
 
-    public Response generateQB(Map<String, Object> map){
+    public Response generateQB(Map<String, Object> map) {
         Response resp = null;
-        Map<String,String> headerParam = new HashMap<String,String>();
-        try{
+        Map<String, String> headerParam = new HashMap<String, String>();
+        try {
             resp = HttpRestUtil.makePostRequest(ML_URL, map, headerParam);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return resp;
     }
 
-    public Response search(Map<String, Object> map){
-        Response resp = null;
-        return resp;
+    public Response search(Map<String, Object> map) {
+        Response contentResponse = null;
+        Map<String, String> headerParam = new HashMap<String, String>();
+        List<Map<String, Object>> contents = new ArrayList<>();
+        //Map<String, Object> params = new HashMap<>();
+        try {
+            contentResponse = HttpRestUtil.makePostRequest(LP_SEARCH, map, headerParam);
+            if ("OK".equalsIgnoreCase(contentResponse.getResponseCode().toString())) {
+                contents.addAll((List<Map<String, Object>>) ((Map<String, Object>) contentResponse.getResult()).get("content"));
+                // map.put("objectType","AssessmentItem");
+                //Response itemResponse = HttpRestUtil.makePostRequest(LP_SEARCH, map, headerParam);
+                //contents.addAll((List<Map<String,Object>>)((Map<String,Object>)itemResponse.getResult()).get("items"));
+            }
+            Map<String, Object> resDataMap = createStructureBasedOnTopic(contents);
+            contentResponse.getResult().remove("content");
+            contentResponse.getResult().put("topics", resDataMap);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return contentResponse;
+    }
+
+    public Map<String, Object> createStructureBasedOnTopic(List<Map<String, Object>> contentList) {
+        Map<String, Map<String, List<Map<String, Object>>>> outPut = new HashMap<>();
+        Map<String, Object> finalMap = new HashMap<>();
+
+        for (int i = 0; i < contentList.size(); i++) {
+            Map<String, Object> contentInnerMap = contentList.get(i);
+            Object topic = (Object) contentInnerMap.get("topic");
+            if (topic != null) {
+                List<String> topicList = (List<String>) topic;
+                for (String topicKey : topicList) {
+                    if (outPut.containsKey(topicKey)) {
+                        Map<String, List<Map<String, Object>>> tempMap = outPut.get(topicKey);
+                        Object keyWords = (Object) contentInnerMap.get("keywords");
+                        if (keyWords != null) {
+                            for (String keyword : (List<String>) keyWords) {
+                                if (!getPreDefinekeyWordList().contains(keyword))
+                                    continue;
+                                if (tempMap.containsKey(keyword)) {
+                                    List<Map<String, Object>> keyWordList = tempMap.get(keyword);
+                                    keyWordList.add(contentInnerMap);
+                                } else {
+                                    List<Map<String, Object>> keyWordList = new ArrayList<>();
+                                    keyWordList.add(contentInnerMap);
+                                    tempMap.put(keyword, keyWordList);
+                                }
+                            }
+                        }
+
+                    } else {
+                        Map<String, List<Map<String, Object>>> tempMap = new HashMap<>();
+                        List<Map<String, Object>> list = new ArrayList<>();
+                        Object keyWords = (Object) contentInnerMap.get("keywords");
+                        if (keyWords != null) {
+                            List<String> keywordList = (List) keyWords;
+                            for (String keyword : keywordList) {
+                                if (!getPreDefinekeyWordList().contains(keyword))
+                                    continue;
+                                list.add(contentInnerMap);
+                                tempMap.put(keyword, list);
+                            }
+                        }
+                        outPut.put(topicKey, tempMap);
+                    }
+                }
+            }
+        }
+        String value = null;
+        try {
+            value = mapper.writeValueAsString(outPut);
+            finalMap = mapper.readValue(value, Map.class);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return finalMap;
+    }
+
+    public static List<String> getPreDefinekeyWordList() {
+        List<String> keyWordsStaticList = new ArrayList<>();
+        keyWordsStaticList.add("dc_primary");
+        keyWordsStaticList.add("dc_secondary");
+        keyWordsStaticList.add("dc_preparatory");
+        keyWordsStaticList.add("dc_practice");
+        keyWordsStaticList.add("dc_assessment");
+        return keyWordsStaticList;
     }
 
 }
